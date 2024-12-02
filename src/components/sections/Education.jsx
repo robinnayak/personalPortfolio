196e4fc2ce36ba0d1d2755ff63710c1672172e3e
robinnayak <robinnayak86@gmail.com>
2024-12-02 11:58:50 +0545

    education part updated

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -2,9 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 
-const Education = ({bg_color}) => {
+const Education = ({ bg_color }) => {
   return (
-    <div className={`container rounded-b-2xl   max-w-full ${bg_color} relative overflow-hidden mb-5 py-8 px-4 rounded-xl`}>
+    <div
+      className={`container rounded-b-2xl   max-w-full ${bg_color} relative overflow-hidden mb-5 py-8 px-4 rounded-xl`}
+    >
       {/* Education Icon */}
       <div className="flex flex-col items-center justify-center mb-8">
         <div className="bg-white p-4 rounded-full shadow-lg animate-bounce">
@@ -13,7 +15,7 @@ const Education = ({bg_color}) => {
             className="text-dark text-4xl"
           />
         </div>
-          <h1 className="text-3xl font-bold text-light">Education</h1>
+        <h1 className="text-3xl font-bold text-light">Education</h1>
       </div>
 
       {/* Timeline Container */}
@@ -22,23 +24,24 @@ const Education = ({bg_color}) => {
         <div className="min-w-full flex justify-evenly md:items-start">
           {/* First Education Entry */}
           <div className="flex flex-col mt-5">
+            {/* Second Education Entry */}
+            <div className="mb-8 md:mb-0 ">
+              <h1 className="text-xl md:text-2xl font-bold text-white">
+                Nepal Engineering College{" "}
+              </h1>
+              <p className="text-light text-sm md:text-base mt-2">
+                Passed Year: 2019-2024{" "}
+                <span className="text-primary">Graduated</span>
+              </p>
+            </div>
+
             <div className="mb-5 md:mb-0">
               <h1 className="text-xl md:text-2xl font-bold text-white">
                 Delhi Public School
               </h1>
               <p className="text-light text-sm md:text-base mt-2">
-                Passed Year: 2016 <span className="text-primary">10th Grade</span>
-              </p>
-            </div>
-
-            {/* Second Education Entry */}
-            <div className="mb-8 md:mb-0 ">
-              <h1 className="text-xl md:text-2xl font-bold text-white">
-                Nepal Engineering College{" "}
-                
-              </h1>
-              <p className="text-light text-sm md:text-base mt-2">
-                Passed Year: 2019-2024 <span className="text-primary">Graduated</span>
+                Passed Year: 2016{" "}
+                <span className="text-primary">10th Grade</span>
               </p>
             </div>
           </div>
@@ -53,14 +56,13 @@ const Education = ({bg_color}) => {
           <div className="mb-8 mt-5 md:mb-0">
             <h1 className="text-xl md:text-2xl font-bold text-white">
               Mithila Institute of Technology{" "}
-              
             </h1>
             <p className="text-light text-sm md:text-base mt-2">
-              Passed Year: 2017-2019 <span className="text-primary">12th Grade</span>
+              Passed Year: 2017-2019{" "}
+              <span className="text-primary">12th Grade</span>
             </p>
           </div>
         </div>
-
       </div>
     </div>
   );
